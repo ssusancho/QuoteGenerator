47d2db1ed4a42202438239d2e56780040efd2f1f
ssusancho <scho2024@chadwickschool.org>
2022-03-21 10:20:56 -0700

    chore: Worked on implementation

--- Program.cs
+++ Program.cs
@@ -33,51 +33,62 @@ namespace QuoteGenerator
             List<string> sports = new List<string>();
             quotes.Add(sports);
 
+            List<string> quoteNames = new List<string>();
+            quoteNames.Add("Funny");
+            quoteNames.Add("Sad");
+            quoteNames.Add("Inspirational");
+            quoteNames.Add("Nature");
+            quoteNames.Add("Music");
+            quoteNames.Add("Sports");
 
+            int userChoice = DisplayQuoteOptions(quoteNames);
 
-            Console.WriteLine($"{quotes}");
+            List<string> selectedQuotes = quotes[userChoice - 1];
 
-            Console.WriteLine($"Enter quote option: ");
-            string input = Console.ReadLine();
-
-            if (input == "funny")
-            {
-                Random generator = new Random();
-                int index = generator.Next(0, funny.Count);
-                string randomFunny = funny[index];
-                Console.WriteLine($"{randomFunny}");
-            }
-            // else (input == "sad")
-            // {
-            //     Random generator = new Random();
-            //     int index = generator.Next(0, sad.Count);
-            //     string randomSad = sad[index];
-            //     Console.WriteLine($"{randomSad}");
-            // }
+            Random generator = new Random();
+            int index = generator.Next(0, selectedQuotes.Count);
+            string quote = selectedQuotes[index];
+            Console.WriteLine($"{quote}");
 
         }
 
-        public static void DisplayQuoteOptions(List<string> quoteNames)
+        public static int DisplayQuoteOptions(List<string> quoteNames)
         {
 
             if (quoteNames == null) throw new ArgumentException("List of options may not be null.");
             if (quoteNames.Count == 0) throw new ArgumentException("The list of options must contain at least 1 option.");
 
+            
+
             int ix = 1;
-            foreach (string quoteName in quoteNames)
+            foreach (string name in quoteNames)
             {
-                Console.WriteLine($"{ix}. {quoteNames}");
+                
+                Console.WriteLine($"{ix}. {name}");
                 ix = ix + 1;
             }
-        }
-        public static void QuoteAssignName(List<string> quotes, List<double> quoteNames)
-        {
-            foreach (string quote in quotes)
+
+            int userChoice;
+            do{
+            Console.Write("Enter the number in front of your choice:");
+            string input = Console.ReadLine();
+            bool isANumber = int.TryParse(input, out userChoice);
+            if (isANumber == false)
             {
-                if (quote == "funny")
-                {
-                    funny = quoteNames("funny");
-                }
+                Console.Error.WriteLine("You did not enter a number.");
             }
+            else if (userChoice > 6)
+            {
+                Console.Error.WriteLine("That number was greater than the options displayed.");
+            }
+            else if (userChoice <= 0)
+            {
+                Console.Error.WriteLine("That number was less than the options displayed.");
+            }
+            }
+            while (userChoice > 6 || userChoice <= 0);
+            return userChoice;
         }
+        
     }
+}
